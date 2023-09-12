112fcc5e23cbbd509093d6dfe6596f1c26c4dbd7
Pablo <Pabloskys73@hotmail.es>
2023-09-12 10:40:07 +0200

    Second commit

--- app/src/main/java/com/example/aplicacion1/MainActivity.java
+++ app/src/main/java/com/example/aplicacion1/MainActivity.java
@@ -40,8 +40,8 @@ public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onDestroy(){
-        super.onDestroy();
         Log.d("onDestroy", "Pablo Rodriguez Hernandez");
+        super.onDestroy();
     }
 
 }
